7a4659333fefaf4cb7568441d1494fc992b4bfb0
Joao Gardin <gardin1992@gmail.com>
2019-10-06 16:40:18 -0300

    Base Model e Controller

---
+++ Controllers/FuncionarioController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class FuncionarioController : Controller
+    {
+        private readonly IFuncionarioDAL funci;
+
+        public FuncionarioController(IFuncionarioDAL logger)
+        {
+            funci = logger;
+        }
+
+        public IActionResult Index()
+        {
+            List<Funcionario> listaFuncionarios = new List<Funcionario>();
+            listaFuncionarios = funci.GetAllFuncionarios().ToList();
+            return View(listaFuncionarios);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Funcionario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication2.Models
+{
+    [Table("Funcionarios")]
+    public class Funcionario
+    {
+        [Key]
+        public int FuncionarioId { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        public string Cidade { get; set; }
+
+        [Required]
+        public string Departamento { get; set; }
+
+        [Required]
+        public string Sexo { get; set; }
+    }
+}
